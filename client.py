0b4b7413d1655e675393a31d8f15a6f1d3a48358
Ana Cuevas Bravo <acuevas@gamma17.aulas.gsyc.urjc.es>
2017-10-23 14:57:11 +0200

    apartado 6

--- client.py
+++ client.py
@@ -8,18 +8,20 @@ import socket
 import sys
 
 # Leemos los parámatros
-if len(sys.argv) < 4:
-        sys.exit("Usage: python3 client.py ip port line")
+if len(sys.argv) != 5:
+        sys.exit("Usage: python3 client.py ip port register username")
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])
+User = sys.argv[4]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+if sys.argv[3] == 'register':
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, PORT))
+        print("Enviando:register")
+        my_socket.send(bytes('REGISTER sip: ', 'utf-8') + 
+                       bytes(User, 'utf-8') + bytes(' SIP/2.0\r\n\r\n', 'utf-8'))
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
 
-print("Socket terminado.")
+    print("Socket terminado.")
