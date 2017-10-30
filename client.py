297bf1816b462d3303574ad0620ec3c1c984258f
Ana Cuevas Bravo <acuevas@gamma08.aulas.gsyc.urjc.es>
2017-10-30 15:22:34 +0100

    corregido con pep8

--- client.py
+++ client.py
@@ -9,7 +9,8 @@ import sys
 
 # Leemos los parámatros
 if len(sys.argv) != 6:
-        sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+        sys.exit('Usage: client.py ip puerto register' +
+                 'sip_address expires_value')
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 User = sys.argv[4]
@@ -19,7 +20,7 @@ if sys.argv[3] == 'register':
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((SERVER, PORT))
-        my_socket.send(bytes('REGISTER sip:' + User + ' SIP/2.0\r\n' + 
+        my_socket.send(bytes('REGISTER sip:' + User + ' SIP/2.0\r\n' +
                        'Expires:' + sys.argv[5] + '\r\n\r\n', 'utf-8'))
         data = my_socket.recv(1024)
         print(data.decode('utf-8'))
